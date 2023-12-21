ade96425e9aa0c9b885208231417ddd7be142d51
J-Ajsivinac <3114791110409@ingenieria.usac.edu.gt>
2023-12-21 15:47:01 -0600

    se inicia con el servidor

--- SegundaF/backend/go.mod
+++ SegundaF/backend/go.mod
@@ -1,4 +1,4 @@
-module SegundaF
+module backend
 
 go 1.21.4
 
--- SegundaF/backend/main.go
+++ SegundaF/backend/main.go
@@ -6,8 +6,13 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+func helloWorld(c *fiber.Ctx) error {
+	return c.SendString("Hello, World!")
+}
+
 func main() {
 	fmt.Println("Hello World")
 	app := fiber.New()
+	app.Get("/", helloWorld)
 	app.Listen(":3000")
 }
